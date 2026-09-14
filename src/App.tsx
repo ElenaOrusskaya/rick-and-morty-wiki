68e3b13c07df787449635fcb197393b84a59599f
Elena <elenaorowsski@gmail.com>
2026-09-14 14:30:08 +0300

    fix: switch to HashRouter and handle wildcard redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-/*import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-*/
 import './App.scss';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {HashRouter, Routes, Route, Navigate} from 'react-router-dom';
 
 import { CharacterPage } from './components/CharacterPage/CharacterPage';
 import {Header} from './components/Header/Header';
@@ -12,42 +8,20 @@ import {Location} from './Pages/Location/Location';
 
 
 function App() {
-  /* const [count, setCount] = useState(0) */
 
   return (
 <>
-<BrowserRouter>
+<HashRouter>
   <Header />
 
   <Routes>
     <Route path = "/" element = {<CharacterPage />} />
     <Route path = "/episodes" element = {<Episodes />} />
     <Route path = "/location" element = {<Location />} />
+    <Route path="*" element={<Navigate to="/" replace />} />
   </Routes>
-</BrowserRouter>
+</HashRouter>
 </>
-   /* <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>*/
   )
 }
 
